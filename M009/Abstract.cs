ec114ebbc7f6543f396e834190635cb7fe1bd454
ppedvLukasK <lukask@ppedv.de>
2022-04-07 11:59:59 +0200

    M009

---
+++ M009/Abstract.cs
@@ -0,0 +1,72 @@
+﻿namespace M009;
+
+public class Abstract
+{
+	static void Main(string[] args)
+	{
+		Mensch m = new Mensch("Hans", 33);
+		Hund h = new Hund("Bello");
+
+		List<Lebewesen> lebewesens = new List<Lebewesen>(); //Unterschiedliche Unterklassen in die selbe Liste geben
+		lebewesens.Add(m);
+		lebewesens.Add(h);
+		
+		foreach (Lebewesen l in lebewesens)
+		{
+			l.PrintStatus(); //Abstrakte Methode bei jedem Objekt ausführen
+		}
+	}
+}
+
+public abstract class Lebewesen //abstract um Objekt Erstellung zu verhindern
+{
+	/// <summary>
+	/// Der Name des Lebewesens
+	/// </summary>
+	public string Name { get; set; }
+
+	public Lebewesen(string name)
+	{
+		Name = name;
+	}
+
+	/// <summary>
+	/// Gibt den Status des Lebewesens aus
+	/// </summary>
+	public abstract void PrintStatus(); //abstrakte Methode ohne Body (mit ; am Ende)
+}
+
+public class Mensch : Lebewesen //Strg + . auf Klassenname um alle abstrakten Member zu implementieren
+{
+	public int Alter { get; set; }
+
+	public Mensch(string name, int alter) : base(name)
+	{
+		Alter = alter;
+	}
+
+	/// <summary>
+	/// Gibt Name und Alter aus
+	/// </summary>
+	public override void PrintStatus() //Unterklasse muss abstrakte Methode überschreiben
+	{
+		Console.WriteLine($"Mensch: {Name} {Alter}");
+	}
+}
+
+public class Hund : Lebewesen
+{
+	/// <summary>
+	/// Der Standardkonstruktor des Lebewesens
+	/// </summary>
+	/// <param name="name">Der Name des Lebewesens</param>
+	public Hund(string name) : base(name)
+	{
+
+	}
+
+	public override void PrintStatus()
+{
+		Console.WriteLine($"Hund: {Name}");
+	}
+}
---
+++ M009/Program.cs
@@ -0,0 +1,71 @@
+﻿public class Program
+{
+	static void Main2(string[] args)
+	{
+		Lebewesen lw = new Mensch("Hans", 27); //Mensch auf Lebewesen Variable zuweisen
+
+		if (lw.GetType() == typeof(Lebewesen)) //GetType() holt den Laufzeittypen (Mensch)
+		{
+			//false
+		}
+
+		if (lw.GetType() == typeof(Mensch)) //typeof(Type): holt ein Type Objekt aus einem Klassennamen
+		{
+			//true
+		}
+
+		if (lw is Lebewesen) //Schauen ob Typ in Vererbungshierarchie ist
+		{
+			//true
+		}
+
+		if (lw is Mensch)
+		{
+			//true
+		}
+
+
+		Mensch m = new Mensch("Peter", 55);
+		Lebewesen lw2 = (Lebewesen) m; //lw2 weis das m ein Mensch ist
+
+		lw2.PrintStatus(); //PrintStatus() von Mensch
+
+		#region Methode überschreiben mit new
+		Mensch m2 = new Mensch("Gustav", 44);
+		m2.PrintStatus();
+
+		Lebewesen lw3 = (Lebewesen) m2; //new: Verbindung trennen zwischen Ober- und Unterklasse
+		lw3.PrintStatus();
+		#endregion
+	}
+}
+
+public class Lebewesen
+{
+	public string Name { get; set; }
+
+	public Lebewesen(string name)
+	{
+		Name = name;
+	}
+
+	public virtual void PrintStatus()
+	{
+		Console.WriteLine(Name);
+	}
+}
+
+public class Mensch : Lebewesen
+{
+	public int Alter { get; set; }
+
+	public Mensch(string name, int alter) : base(name)
+	{
+		Alter = alter;
+	}
+
+	public new void PrintStatus()
+	{
+		Console.WriteLine($"{Name} {Alter}");
+	}
+}
